6e9f1554dbae919dbc8bf9d492cca4cd6d5753bf
mverbytska <verbitskaya.mari@gmail.com>
2024-03-14 16:51:03 +0100

    [Create] I/O functions using different capabilities

---
+++ app/io/input.py
@@ -0,0 +1,12 @@
+
+def get_input_from_console():
+    pass
+
+
+def input_using_built_in_funcs():
+    pass
+
+
+def input_using_pandas_lib():
+    pass
+
---
+++ app/io/output.py
@@ -0,0 +1,11 @@
+
+def output_in_console():
+    pass
+
+
+def file_output_with_built_in_funcs():
+    pass
+
+
+def file_output_with_pandas_lib():
+    pass
