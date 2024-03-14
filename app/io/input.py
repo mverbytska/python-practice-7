57c65d22ceac42bbeb9d78b6d3e923fad1e236f7
mverbytska <verbitskaya.mari@gmail.com>
2024-03-14 17:38:45 +0100

    [Modify] create docstrings for I/O functions

--- app/io/input.py
+++ app/io/input.py
@@ -1,12 +1,44 @@
 
 def get_input_from_console():
+    """
+    Gets user's input from console using built-in function input(), and returns it.
+
+    Returns:
+        :return: User's value that was written into the input function as an argument.
+        :rtype: str
+    """
     pass
 
 
 def input_using_built_in_funcs():
+    """
+    Gets input from file, using Python's built-in file reader, of mentioned filename,
+    and returns it.
+    Args:
+        param filename(str): String with file's name from which the input is read.
+    Returns:
+        :return:Text from input file.
+        :rtype: str
+
+    Raises:
+        FileNotFoundError if input file does not exist or is not found.
+    """
     pass
 
 
 def input_using_pandas_lib():
+    """
+    Gets input from file, using Pandas library, of mentioned filename,
+    and returns it.
+
+    Args;
+        param filename(str): String with file' name from which the input is read.
+    Returns:
+        :return: Text from input file.
+        :rtype: str
+
+    Raises:
+        FileNotFoundError if input file does not exist or is not found.
+    """
     pass
 
--- app/io/output.py
+++ app/io/output.py
@@ -1,11 +1,42 @@
 
 def output_in_console():
+    """
+    Prints given output in console.
+    Args:
+        param text(str): Text to print into console
+    :return: None
+
+    Examples:
+    --------
+
+    >>> output_in_console('mariia')
+    mariia
+
+    >>>output_in_console('123')
+    123
+    """
     pass
 
 
 def file_output_with_built_in_funcs():
+    """
+    Writes the given output into file with a given filename using Python built-in
+    file writer.
+    Args:
+        param text(str): Text to write into console.
+        param filename(str): Name of file into which the output is written.
+    :return: None
+    """
     pass
 
 
 def file_output_with_pandas_lib():
+    """
+    Writes the given output into .csv format file with a given filename
+     using Pandas function
+    Args:
+        param text(str): Text to write into console.
+        param filename(str): Name of file into which the output is written.
+    :return: None
+    """
     pass
