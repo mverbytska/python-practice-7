63b0d69b2d2ea2a73e72a607f3ed0f6f59bc6ff5
mverbytska <verbitskaya.mari@gmail.com>
2024-03-15 10:04:28 +0100

    [Create] test for reading input using pandas lib from non-existing file

--- tests/test_io/test_input.py
+++ tests/test_io/test_input.py
@@ -1,15 +1,12 @@
 import unittest
-import os
 from app.io import input
 
 
-tests_script_dir = os.path.dirname(os.path.abspath(__file__))
-my_static_data_dir = os.path.join(tests_script_dir, '..', '..', 'my_static_data')
-error_filepath = os.path.join(my_static_data_dir, 'error_file.txt')
+error_filepath = './error_file.txt'
 
 
 class TestInputWithBuiltInFunctions(unittest.TestCase):
-    def test_input_string_using_built_func_check_not_existing_file_(self):
+    def test_input_string_using_built_func_check_non_existing_file_(self):
         with self.assertRaises(FileNotFoundError):
             input.input_using_built_in_funcs(error_filepath)
 
@@ -19,6 +16,14 @@ class TestInputWithBuiltInFunctions(unittest.TestCase):
 
         test_result = input.input_using_built_in_funcs('./temp_file.txt')
         self.assertEqual(test_result, 'Let\'s test this function!')
+
+
+class TestInputWithPandasLibrary(unittest.TestCase):
+
+    def test_input_using_pandas_with_non_existing_file(self):
+        with self.assertRaises(FileNotFoundError):
+            input.input_using_pandas_lib(error_filepath)
+
 
 
 if __name__ == '__main__':
